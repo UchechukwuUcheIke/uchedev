7bcb6633bef30d3cf9d430839d236431e39efb02
Uche <uuchei2@illinois.edu>
2024-05-31 19:54:09 -0500

    Feat: Added MaterialUI Styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,11 @@
 import { Metadata } from 'next'
-import { AppCacheProvider } from '@mui/material-nextjs/v13-pagesRouter';
  
 export const metadata: Metadata = {
   title: 'Uchedev',
 }
 
-export default function Home(props) {
+export default function Home() {
   return (
-    <AppCacheProvider {...props}>
       <h1>Hi from Home</h1>
-+    </AppCacheProvider>
   );
 }
