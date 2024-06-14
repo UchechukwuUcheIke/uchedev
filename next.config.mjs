4bca859f578250b1a5fbb42df1367a354132f163
Uche <uuchei2@illinois.edu>
2024-06-14 13:30:21 -0500

    Sanity check

--- next.config.mjs
+++ next.config.mjs
@@ -6,12 +6,6 @@ const nextConfig = {
   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
   // Optionally, add any other Next.js config below
 }
- 
-const withMDX = createMDX({
-  // Add markdown plugins here, as desired
-  options: {
-  },
-})
- 
+
 // Wrap MDX and Next.js config with each other
-export default withMDX(nextConfig)
+export default nextConfig;
