5583ad89f133be243cb94f61eb22c5e46afcb111
Uche <uuchei2@illinois.edu>
2024-06-15 09:22:10 -0500

    Reverting back to mdx

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,17 @@
-// @ts-check
+import createMDX from '@next/mdx'
  
-/**
- * @type {import('next').NextConfig}
- */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   // Configure `pageExtensions`` to include MDX files
   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+  // Optionally, add any other Next.js config below
 }
  
-export default nextConfig
+const withMDX = createMDX({
+  // Add markdown plugins here, as desired
+  options: {
+  },
+})
+ 
+// Wrap MDX and Next.js config with each other
+export default withMDX(nextConfig)
--- src/app/blog/megaman-x-vs-zero/page.tsx
+++
@@ -1,7 +0,0 @@
-import Markdown from 'react-markdown'
-
-const Article = () => {
-    return <Markdown>{`# Hello markdown!`}</Markdown>;
-};
-
-export default Article
