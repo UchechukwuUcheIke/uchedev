c4e5c23728fd4ae13af351655021d43f6b4fd2da
Uche <uuchei2@illinois.edu>
2024-06-15 09:44:47 -0500

    Reverting back to mdx

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,18 @@
 import createMDX from '@next/mdx'
- 
+   
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Configure `pageExtensions`` to include MDX files
+  // Configure `pageExtensions` to include MDX files
   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+  experimental: {
+    mdxRs: true,
+  },
   // Optionally, add any other Next.js config below
 }
- 
+
 const withMDX = createMDX({
   // Add markdown plugins here, as desired
-  options: {
-  },
 })
- 
+
 // Wrap MDX and Next.js config with each other
-export default createMDX(nextConfig)
+export default withMDX(nextConfig)
