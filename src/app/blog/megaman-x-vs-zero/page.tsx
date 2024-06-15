6bcdd8f0586e561ac22c7b87cadac1081befc814
Uche <uuchei2@illinois.edu>
2024-06-15 14:44:13 -0500

    Fixed ESLint Error"

--- src/app/blog/megaman-x-vs-zero/page.tsx
+++ src/app/blog/megaman-x-vs-zero/page.tsx
@@ -1,12 +1,8 @@
 import Markdown from 'react-markdown'
 
 //TODO: Find a better way to store article content!
-export default function Article({children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function Article() {
   return (
       <Markdown>{"Hi!"}</Markdown>
-
   )
 }
