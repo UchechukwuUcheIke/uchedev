8253a971e53333c94eb44583fa989fcbf934229f
Uche <uuchei2@illinois.edu>
2024-06-15 20:21:50 -0500

    Sanity Check

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -112,7 +112,7 @@ export default function Homepage() {
         <ContentCard
           title="From Megman X to Zero: Leveling Up and Falling Short"
           subheader="June 3rd, 2024"
-          imageURL={basePath + "/images/MaverickHunterX.png"}
+          imageURL={"uchedev/images/MaverickHunterX.png"}
           imageAlt={"From Megman X to Zero Thumbnail"}>
             Analysis of Megaman X and Megaman Zero games
         </ContentCard>
