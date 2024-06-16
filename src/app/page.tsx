2490f3a91bed850a76e08b13f3c7e0436a624c0d
Uche <uuchei2@illinois.edu>
2024-06-15 20:11:42 -0500

    Sanity Check

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -69,7 +69,7 @@ export default function Homepage() {
               flexGrow: 1,
             }}
             alt="Portrait of Uche"
-            src={basePath + "/images/portrait.png"}
+            src={"uchechukwuucheike.github.io/uchedev/images/portrait.png"}
           />
           
         </Stack>
